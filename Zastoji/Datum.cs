4d0b317764002aa559c0dca2f2e14f44e7828e44
NJUFRA-PC\Darko <darko.eler@petrokemija.hr>
2019-06-14 11:28:42 +0200

    Prije.Net3,5

--- Zastoji/Datum.cs
+++ Zastoji/Datum.cs
@@ -1,25 +1,24 @@
 ﻿namespace Zastoji
 {
-    public class Datum
-    {
-        public int Godine { get; }
-        public int Mjeseci { get; }
-        public int Dani { get; }
-        public int Sati { get;}
-        public int Minute { get;}
+   public class Datum
+   {
+      public Datum()
+      {
+      }
 
-        public Datum()
-        {
-            
-        }
+      public Datum(int godine, int mjeseci, int dani, int sati, int minute)
+      {
+         Godine = godine;
+         Mjeseci = mjeseci;
+         Dani = dani;
+         Sati = sati;
+         Minute = minute;
+      }
 
-        public Datum(int godine, int mjeseci, int dani, int sati, int minute)
-        {
-            Godine = godine;
-            Mjeseci = mjeseci;
-            Dani = dani;
-            Sati = sati;
-            Minute = minute;
-        }
-    }
+      public int Godine { get; }
+      public int Mjeseci { get; }
+      public int Dani { get; }
+      public int Sati { get; }
+      public int Minute { get; }
+   }
 }
--- Zastoji/MainWindow.xaml.cs
+++ Zastoji/MainWindow.xaml.cs
@@ -3,65 +3,103 @@ using System.Windows;
 
 namespace Zastoji
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public DateTime PocetnoVrijeme { get; set; }
-        public DateTime ZavrsnoVrijeme { get; set; }
-        public TimeSpan Razlika { get; set; }
-        public MainWindow()
-        {
-            InitializeComponent();           
-                      
-        }
+   /// <summary>
+   ///    Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      private double _rad;
+      private double _zastoj;
 
-        private void IzracunajVrijeme_Click(object sender, RoutedEventArgs e)
-        {
-           Datum razlika = null;
-            if (DtPocetnoVrijeme.Value == null || DtZavrnoVrijeme.Value == null || DtPocetnoVrijeme.Value > DtZavrnoVrijeme.Value)return;
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
 
-              PocetnoVrijeme = (DateTime)DtPocetnoVrijeme.Value;             
-              ZavrsnoVrijeme= (DateTime)DtZavrnoVrijeme.Value;
-               razlika = new RazlikaDatuma(PocetnoVrijeme, ZavrsnoVrijeme).VratiIzracun();
-            IspisiULabele(razlika);
-            if (razlika.Dani > 0)
-            {
-               var sati = (razlika.Dani * 24) + razlika.Sati;
+      public DateTime PocetnoVrijeme { get; set; }
+      public DateTime ZavrsnoVrijeme { get; set; }
+      public TimeSpan Razlika { get; set; }
 
-                Rezultat.Content  = sati +" sati, " + razlika.Minute + " minute";
-            }
-            else
-            {
-                Rezultat.Content  =  razlika.Sati+" sati, " + razlika.Minute + " minute";
-            }          
-            
-        }
+      private void IzracunajVrijeme_Click(object sender, RoutedEventArgs e)
+      {
+         Datum razlika = null;
+         if (DtPocetnoVrijeme.Value == null || DtZavrnoVrijeme.Value == null ||
+             DtPocetnoVrijeme.Value > DtZavrnoVrijeme.Value) return;
+
+         PocetnoVrijeme = (DateTime) DtPocetnoVrijeme.Value;
+         ZavrsnoVrijeme = (DateTime) DtZavrnoVrijeme.Value;
+         razlika = new RazlikaDatuma(PocetnoVrijeme, ZavrsnoVrijeme).VratiIzracun();
+         IspisiULabele(razlika);
+         if (razlika.Dani > 0)
+         {
+            var sati = razlika.Dani * 24 + razlika.Sati;
+
+            Rezultat.Content = sati + " sati, " + razlika.Minute + " minute";
+            DodajUListu(sati, razlika.Minute);
+         }
+         else
+         {
+            Rezultat.Content = razlika.Sati + " sati, " + razlika.Minute + " minute";
+            DodajUListu(razlika.Sati, razlika.Minute);
+         }
+      }
+
+      private void DodajUListu(int sati, int razlikaMinute)
+      {
+         double.TryParse(sati + "," + razlikaMinute, out var satmin);
+         if (RadBtnZastoj.IsChecked == true)
+         {
+            ListaZastoja.Items.Add(satmin);
+            _zastoj += satmin;
+            UkupnoZastoja.Content = "Ukupno: " + _zastoj;
+         }
+         else
+         {
+            ListaRada.Items.Add(satmin);
+            _rad += satmin;
+            UkupnoRada.Content = "Ukupno: " + _rad;
+         }
+
+         //double total += satmin;
+      }
 
       private void IspisiULabele(Datum razlika)
       {
-             LblMinute.Content = razlika.Minute;
-            var sati = razlika.Sati;
-            var dani = razlika.Dani;
-            if (sati < 0)
-            {
-                dani = dani -1;
-                LblDani.Content = dani;
-                sati = (-24 - razlika.Sati) * (-1);
-                LblSati.Content = sati;
-            }
-            else
-            {
+         LblMinute.Content = razlika.Minute;
+         var sati = razlika.Sati;
+         var dani = razlika.Dani;
+         if (sati < 0)
+         {
+            dani = dani - 1;
+            LblDani.Content = dani;
+            sati = (-24 - razlika.Sati) * -1;
+            LblSati.Content = sati;
+         }
+         else
+         {
             LblDani.Content = razlika.Dani;
             LblSati.Content = razlika.Sati;
-            }
+         }
       }
 
       private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            DtPocetnoVrijeme.Value = DateTime.Now;
-            DtZavrnoVrijeme.Value = DateTime.Now;
-        }
-    }
+      {
+         DtPocetnoVrijeme.Value = DateTime.Now;
+         DtZavrnoVrijeme.Value = DateTime.Now;
+      }
+
+      private void BtnOcistiListuZastoja_Click(object sender, RoutedEventArgs e)
+      {
+         ListaZastoja.Items.Clear();
+         _zastoj = 0.0;
+         UkupnoZastoja.Content = "";
+      }
+
+      private void BtnOcistiListuRada_Click(object sender, RoutedEventArgs e)
+      {
+         ListaRada.Items.Clear();
+         _rad = 0.0;
+         UkupnoRada.Content = "";
+      }
+   }
 }
